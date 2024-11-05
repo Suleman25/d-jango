be048c37a800ec33876adaba1d19c92bcd1f8858
Suleman25 <sulemanjamil177@gmail.com>
2024-11-05 15:56:49 +0500

    2

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('firstapp/' , include('firstapp.urls')),
-    path('secondapp/' , include('secondapp.urls')),
+    # path('admin/', admin.site.urls),
+    # path('firstapp/' , include('firstapp.urls')),
+    path('secondapp/', include('secondapp.urls')),
 ]
--- backend/secondapp/views.py
+++ backend/secondapp/views.py
@@ -2,29 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def index(request , months):
+    if months == 'jan':
+        return HttpResponse('<h1>hello world</h1>')
+    elif months == 'dec':
+        return HttpResponse('<h1>Winter Vacations</h1>')
+    else :
+        return HttpResponse('<h1>URL not found</h1>')
 
-def jan(request):
-    return HttpResponse('<i>Front-End</i>')
-def feb(request):
-    return HttpResponse('<i>Back-End</i>')
-def mar(request):
-    return HttpResponse('<i>React</i>')
-def apr(request):
-    return HttpResponse('<i>DBMS</i>')
-def may(request):
-    return HttpResponse('<i>Python</i>')
-def jun(request):
-    return HttpResponse('<i>Node js</i>')
-def jul(request):
-    return HttpResponse('<i>Flutter & Dart</i>')
-def aug(request):
-    return HttpResponse('<i>Machine Learning</i>')
-def sep(request):
-    return HttpResponse('<i>Git & Github</i>')
-def oct(request):
-    return HttpResponse('<i>DJango</i>')
-def nov(request):
-    return HttpResponse('<i>BootStrap</i>')
-def dec(request):
-    return HttpResponse('<i>Mobile App Development</i>')
+
+# def jan(request):
+#     return HttpResponse('<i>Front-End</i>')
+# def feb(request):
+#     return HttpResponse('<i>Back-End</i>')
+# def mar(request):
+#     return HttpResponse('<i>React</i>')
+# def apr(request):
+#     return HttpResponse('<i>DBMS</i>')
+# def may(request):
+#     return HttpResponse('<i>Python</i>')
+# def jun(request):
+#     return HttpResponse('<i>Node js</i>')
+# def jul(request):
+#     return HttpResponse('<i>Flutter & Dart</i>')
+# def aug(request):
+#     return HttpResponse('<i>Machine Learning</i>')
+# def sep(request):
+#     return HttpResponse('<i>Git & Github</i>')
+# def oct(request):
+#     return HttpResponse('<i>DJango</i>')
+# def nov(request):
+#     return HttpResponse('<i>BootStrap</i>')
+# def dec(request):
+#     return HttpResponse('<i>Mobile App Development</i>')
 
